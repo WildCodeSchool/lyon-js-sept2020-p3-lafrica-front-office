ca33b4a27901c2a7d1999c9dc523b9ab299b4bc2
Rom-mtl <romain.montel@gmail.com>
2020-12-04 10:54:54 +0100

    Adding SingIn and SignUp Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import "./App.css";
-import Footer from "./footer/Footer";
-import Header from "./header/Header";
+import './App.css';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+import SignIn from './login/SignIn';
+
+import SingUp from './login/SignUp';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
+      <SignIn />
+      <SingUp />
       <Footer />
     </div>
   );
