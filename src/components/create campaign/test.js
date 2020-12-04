e87ec8ffcb196686a7b21c8d92d825aeb130ee3a
Guillem <guillem.dardill@gmail.com>
2020-12-04 11:22:07 +0100

    CSS added

--- src/components/create campaign/test.js
+++ src/components/create campaign/test.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Slider from '@material-ui/core/Slider';
+import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/core/Slider";
 
 const useStyles = makeStyles((theme) => ({}));
 
 const CustomSlider = withStyles({
   root: {
-    color: 'rgb(98, 186, 227)',
+    color: "rgb(98, 186, 227)",
   },
 })(Slider);
 
@@ -17,8 +17,8 @@ export default function CustomizedSlider() {
     <div className={classes.root}>
       <div className={classes.margin} />
       <CustomSlider
-        valueLabelDisplay='auto'
-        aria-label='pretto slider'
+        valueLabelDisplay="auto"
+        aria-label="pretto slider"
         defaultValue={20}
       />
     </div>
