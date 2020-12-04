ef9ef10e686107b381f15a49b0706f997d5982dd
Guillem <guillem.dardill@gmail.com>
2020-12-04 08:08:37 +0100

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
-import "./App.css";
-
+import './App.css';
+import CreateCampaign from './CreateCampaign';
 
 function App() {
-  return <div className="App">Wild P3</div>;
+  return (
+    <div className='App'>
+      <CreateCampaign />
+    </div>
+  );
 }
 
 export default App;
