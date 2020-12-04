7460eec2942c82edc9446400a2728bbaa1d61f10
cleroy117 <ce.leroy69@gmail.com>
2020-12-04 09:55:31 +0100

    HTML & CSS skeleton is done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
-
+import CampaignsView from "./CampaignsView";
 
 function App() {
-  return <div className="App">Wild P3</div>;
+  return (
+    <div className="App">
+      Wild P3
+      <CampaignsView />
+    </div>
+  );
 }
 
 export default App;
