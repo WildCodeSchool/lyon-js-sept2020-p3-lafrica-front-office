387b3aa0eac1f3d8778fd7f0659ceb609ca97a12
Rom-mtl <romain.montel@gmail.com>
2020-12-08 11:33:26 +0100

    fixed bug

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,30 +5,28 @@ import './home.css';
 
 const Home = () => {
   return (
-    <body>
-      <div className='bodyContainer'>
-        <img src='./Logo_LAM_T2V.png' alt='LAM' />
+    <div className='bodyContainer'>
+      <img src='./Logo_LAM_T2V.png' alt='LAM' />
 
-        <nav>
-          <ul>
-            <li>
-              <Link to='/signIn'>
-                <MdAssignmentInd size={150} color='black' />
-                <br />
-                S'identifier
-              </Link>
-            </li>
-            <li>
-              <Link to='/signUp'>
-                <MdAssignment size={150} color='black' />
-                <br />
-                S'enregistrer
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </body>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/signIn'>
+              <MdAssignmentInd size={150} color='black' />
+              <br />
+              S'identifier
+            </Link>
+          </li>
+          <li>
+            <Link to='/signUp'>
+              <MdAssignment size={150} color='black' />
+              <br />
+              S'enregistrer
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
