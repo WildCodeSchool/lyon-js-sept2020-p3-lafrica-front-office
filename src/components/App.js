a1faa7a9024707255975c2fe9b263dde1c8b3a16
Jean Ducret <jean.dcrt@gmail.com>
2020-12-08 15:01:06 +0100

    fix css homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,30 @@
-import './App.css';
-import CampaignDetail from './CampaignDetail/CampaignDetails';
-import CampaignsView from './campaignsView/CampaignsView';
-import Footer from './footer/Footer';
-import Header from './header/Header';
-import SignIn from './login/SignIn';
-import Home from './home/Home';
-import SignUp from './login/SignUp';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./App.css";
+import CampaignDetail from "./CampaignDetail/CampaignDetails";
+import CampaignsView from "./campaignsView/CampaignsView";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import SignIn from "./login/SignIn";
+import Home from "./home/Home";
+import SignUp from "./login/SignUp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div className='app'>
+      <div className="app">
         <Header />
         <main>
           <Switch>
-            <Route exact path='/'>
+            <Route exact path="/">
               <Home />
             </Route>
-            <Route path='/signIn'>
+            <Route path="/signIn">
               <SignIn />
             </Route>
-            <Route path='/signUp'>
+            <Route path="/signUp">
               <SignUp />
             </Route>
-            <Route path='/users/:user_id/campaigns/:campaign_id'>
+            <Route path="/users/:user_id/campaigns/:campaign_id">
               <CampaignDetail />
             </Route>
           </Switch>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { MdAssignmentInd, MdAssignment } from "react-icons/md";
-import "./home.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdAssignmentInd, MdAssignment } from 'react-icons/md';
+import './home.css';
 
 const Home = () => {
   return (
-    <div className="bodyContainer">
-      <img src="./Logo_LAM_T2V.png" alt="LAM" />
+    <div className='bodyContainer'>
+      <img src='./Logo_LAM_T2V.png' alt='LAM' />
 
       <nav>
         <ul>
           <li>
-            <Link to="/signIn">
-              <MdAssignmentInd size={150} color="black" />
+            <Link to='/signIn'>
+              <MdAssignmentInd className='login-register-logo' color='black' />
               <br />
               S'identifier
             </Link>
           </li>
           <li>
-            <Link to="/signUp">
-              <MdAssignment size={150} color="black" />
+            <Link to='/signUp'>
+              <MdAssignment className='login-register-logo' color='black' />
               <br />
               S'enregistrer
             </Link>
