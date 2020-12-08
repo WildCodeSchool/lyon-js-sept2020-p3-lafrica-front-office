dfabfb152fa415919138be3cb1c223ca79849735
Guillem <guillem.dardill@gmail.com>
2020-12-08 11:57:20 +0100

    CSS added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,37 +1,37 @@
-import React from "react";
-import "./header.css";
-import { slide as Menu } from "react-burger-menu";
-import { BsFillPersonFill } from "react-icons/bs";
+import React from 'react';
+import './header.css';
+import { slide as Menu } from 'react-burger-menu';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 const Header = () => {
   return (
     <header>
       <Menu>
-        <div id="home" className="menu-item" href="/">
-          <BsFillPersonFill size={150} color="white" />
+        <div id='home' className='menu-item' href='/'>
+          <BsFillPersonFill size={150} color='white' />
           <p>Bonjour Utilisateur</p>
         </div>
 
-        <a id="user" className="menu-item" href="/about">
+        <a id='user' className='menu-item' href='/about'>
           COMPTE UTILISATEUR
         </a>
-        <a id="sms" className="menu-item" href="/contact">
+        <a id='sms' className='menu-item' href='/contact'>
           SMS
         </a>
-        <a id="voix" className="menu-item" href="/contact">
+        <a id='voix' className='menu-item' href='/contact'>
           VOIX
         </a>
       </Menu>
 
-      <div className="menuBtn">
+      <div className='menuBtn'>
         <ul>
           <li>
-            <a href="?">Accueil</a>
+            <a href='?'>Accueil</a>
           </li>
         </ul>
         <ul>
           <li>
-            <a href="?">Déconnexion</a>
+            <a href='?'>Déconnexion</a>
           </li>
         </ul>
       </div>
