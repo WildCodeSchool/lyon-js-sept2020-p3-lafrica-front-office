2d86997fc55128ce369296629389ca08c1eb715d
Rom-mtl <romain.montel@gmail.com>
2020-12-10 11:30:23 +0100

    Adding Create Campaign route

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,42 +1,40 @@
-import React from "react";
-import "./header.css";
-import { slide as Menu } from "react-burger-menu";
-import { BsFillPersonFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './header.css';
+import { slide as Menu } from 'react-burger-menu';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Menu>
-        <div id="home" className="menu-item" href="/">
-          <BsFillPersonFill size={150} color="white" />
+        <div id='home' className='menu-item' href='/'>
+          <BsFillPersonFill size={150} color='white' />
           <p>Bonjour Utilisateur</p>
         </div>
-        <Link to="/">ACCUEIL</Link>
-        <Link to="/signIn">S'IDENTIFIER</Link>
-        <a id="user" className="menu-item" href="/about">
+        <Link to='/'>ACCUEIL</Link>
+        <Link to='/signIn'>S'IDENTIFIER</Link>
+        {/* <a id='user' className='menu-item' href='/about'>
           ADMINISTRATION
         </a>
-        <a id="user" className="menu-item" href="/about">
+        <a id='user' className='menu-item' href='/about'>
           COMPTE UTILISATEUR
         </a>
-        <a id="sms" className="menu-item" href="/contact">
+        <a id='sms' className='menu-item' href='/contact'>
           SMS
-        </a>
-        <a id="voix" className="menu-item" href="/contact">
-          VOIX
-        </a>
+        </a> */}
+        <Link to='/users/:user_id/createCampaign'>CREER UNE CAMPAGNE</Link>
       </Menu>
 
-      <div className="menuBtn">
+      <div className='menuBtn'>
         <ul>
           <li>
-            <Link to="/">Accueil</Link>
+            <Link to='/'>Accueil</Link>
           </li>
         </ul>
         <ul>
           <li>
-            <a href="?">Déconnexion</a>
+            <a href='?'>Déconnexion</a>
           </li>
         </ul>
       </div>
