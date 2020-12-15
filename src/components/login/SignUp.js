15b7b7a0360f1fa04cf388ff04075855d4c20013
Jean Ducret <jean.dcrt@gmail.com>
2020-12-15 16:26:56 +0100

    create register functionnality with error handling

--- src/components/login/SignUp.js
+++ src/components/login/SignUp.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -13,35 +14,108 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+import API from '../../services/API';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    background: 'white',
+    padding: '13px',
+    border: '1px solid  #bdc3c7',
+    borderRadius: '5px',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: '#5ebbea',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    '&:hover': {
+      backgroundColor: '#096490',
+      color: 'white',
+    },
+    margin: theme.spacing(3, 0, 2),
+    background: '#5ebbea',
+  },
+}));
+
 const SignUp = () => {
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      background: 'white',
-      padding: '13px',
-      border: '1px solid  #bdc3c7',
-      borderRadius: '5px',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: '#5ebbea',
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      '&:hover': {
-        backgroundColor: '#096490',
-        color: 'white',
-      },
-      margin: theme.spacing(3, 0, 2),
-      background: '#5ebbea',
-    },
-  }));
+  const initialState = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  };
+
+  const [userData, setUserData] = useState(initialState);
+  const [errors, setErrors] = useState({
+    firstname: false,
+    lastname: false,
+    email: false,
+    password: false,
+    password_confirmation: false,
+  });
+
+  const handleUserData = (e) => {
+    setUserData((data) => {
+      return { ...data, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleUserErrors = (e) => {
+    if (e.target.value.length === 0) {
+      setErrors((prevErrors) => {
+        return { ...prevErrors, [e.target.name]: true };
+      });
+    } else if (e.target.name === 'email') {
+      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      const isEmailValid = regex.test(e.target.value);
+
+      if (!isEmailValid) {
+        setErrors((prevErrors) => {
+          return { ...prevErrors, [e.target.name]: true };
+        });
+      } else {
+        setErrors((prevErrors) => {
+          return { ...prevErrors, [e.target.name]: false };
+        });
+      }
+    } else if (
+      (e.target.name === 'password' ||
+        e.target.name === 'password_confirmation') &&
+      e.target.value.length < 5
+    ) {
+      setErrors((prevErrors) => {
+        return { ...prevErrors, [e.target.name]: true };
+      });
+    } else if (
+      e.target.name === 'password_confirmation' &&
+      e.target.value !== userData.password
+    ) {
+      setErrors((prevErrors) => {
+        return { ...prevErrors, [e.target.name]: true };
+      });
+    } else {
+      setErrors((prevErrors) => {
+        return { ...prevErrors, [e.target.name]: false };
+      });
+    }
+  };
+
+  const submitUserData = (e) => {
+    e.preventDefault();
+    API.post('/register', userData)
+      .then(() => setUserData(initialState))
+      .catch(console.error);
+  };
 
   const classes = useStyles();
 
@@ -60,13 +134,18 @@ const SignUp = () => {
             <Grid item xs={12} sm={6}>
               <TextField
                 autoComplete="fname"
-                name="firstName"
+                name="firstname"
                 variant="outlined"
                 required
                 fullWidth
                 id="firstName"
                 label="Prénom"
                 autoFocus
+                error={errors.firstname}
+                helperText={errors.firstname && 'Un prénom est obligatoire'}
+                value={userData.firstname}
+                onChange={(e) => handleUserData(e)}
+                onBlur={(e) => handleUserErrors(e)}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -74,10 +153,15 @@ const SignUp = () => {
                 variant="outlined"
                 required
                 fullWidth
-                id="lastName"
+                id="lastname"
                 label="Nom"
-                name="lastName"
+                name="lastname"
                 autoComplete="lname"
+                value={userData.lastname}
+                helperText={errors.lastname && 'Un nom est obligatoire'}
+                error={errors.lastname}
+                onChange={(e) => handleUserData(e)}
+                onBlur={(e) => handleUserErrors(e)}
               />
             </Grid>
             <Grid item xs={12}>
@@ -89,6 +173,11 @@ const SignUp = () => {
                 label="Adresse e-mail"
                 name="email"
                 autoComplete="email"
+                value={userData.email}
+                error={errors.email}
+                helperText={errors.email && 'adresse email invalide'}
+                onChange={(e) => handleUserData(e)}
+                onBlur={(e) => handleUserErrors(e)}
               />
             </Grid>
             <Grid item xs={12}>
@@ -101,6 +190,34 @@ const SignUp = () => {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                value={userData.password}
+                error={errors.password}
+                helperText={
+                  errors.password &&
+                  "Votre mot de passe doit être d'au moins 5 caractères"
+                }
+                onChange={(e) => handleUserData(e)}
+                onBlur={(e) => handleUserErrors(e)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password_confirmation"
+                label="Confirmation du mot de passe"
+                type="password"
+                id="password_confirmation"
+                autoComplete="current-password"
+                value={userData.password_confirmation}
+                error={errors.password_confirmation}
+                helperText={
+                  errors.password_confirmation &&
+                  'Le mot de passe et la confirmation ne sont pas identiques'
+                }
+                onChange={(e) => handleUserData(e)}
+                onBlur={(e) => handleUserErrors(e)}
               />
             </Grid>
             <Grid item xs={12}>
@@ -116,6 +233,7 @@ const SignUp = () => {
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={submitUserData}
           >
             S'enregistrer
           </Button>
