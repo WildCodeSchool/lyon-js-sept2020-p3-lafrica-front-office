80ae9a5fb0d48cfee9c5e90b36a8502d65a9a41d
Rom-mtl <romain.montel@gmail.com>
2021-01-26 17:00:47 +0100

    Stats ok for 2 routes

--- src/components/Stats/Statistics.js
+++ src/components/Stats/Statistics.js
@@ -5,12 +5,25 @@ import { UserContext } from '../../context/UserContext';
 
 const Statistics = () => {
   const { userDetails } = useContext(UserContext);
-  const [globalDatas, setGlobalDatas] = useState('');
+  const [globalDatas, setGlobalDatas] = useState([]);
+  const [sendCallCount, setSendCallCount] = useState(0);
+  const [totalSendCampaigns, setTotalSendCampaigns] = useState(0);
+  const [totalSendCampaignsPerUsers, setTotalSendCampaignsPerUsers] = useState(
+    []
+  );
 
   const datasFromApi = () => {
     API.get(`/users/${userDetails.id}/statistics`).then((res) => {
-      setGlobalDatas(res.data);
-      console.log(globalDatas);
+      setGlobalDatas(res.data.callsDatas);
+      setTotalSendCampaigns(res.data.totalCampaignsSent);
+      setTotalSendCampaignsPerUsers(res.data.totalCampaignsPerUsers);
+      console.log(totalSendCampaignsPerUsers);
+    });
+  };
+
+  const mappingForTotalSendCampaignsPerUser = () => {
+    return totalSendCampaignsPerUsers.map((el) => {
+      return <p>{el.firstname}</p>;
     });
   };
 
@@ -18,7 +31,23 @@ const Statistics = () => {
     datasFromApi();
   }, []);
 
-  return <div>Hello world</div>;
+  useEffect(() => {
+    globalDatas.forEach((el) => {
+      if (el.call_state_id) {
+        setSendCallCount((prevCount) => prevCount + 1);
+      }
+    });
+  }, [globalDatas]);
+
+  return (
+    <div className="stats-container">
+      <h1>Total des appels émis</h1>
+      <p>{sendCallCount}</p>
+      <h1>Total des Campagnes</h1>
+      <p>{totalSendCampaigns}</p>
+      <div>{mappingForTotalSendCampaignsPerUser()}</div>
+    </div>
+  );
 };
 
 export default Statistics;
